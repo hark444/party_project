eedddac4125484fe720a6a10b237c3d5f811c206
Harshad Keshwani <hark44>
2022-08-21 03:46:43 +0530

    Created patch api for role addition

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from .routes import user
+from .routes import user, user_roles
 
 version_router = APIRouter(prefix="/v1")
 version_router.include_router(user.user_router)
+version_router.include_router(user_roles.user_role_router)
