88ebb78f1545a98d45acf7ad4fa55c56cf992d20
Harshad Keshwani <hark44>
2022-10-11 17:06:31 +0530

    Added CORS policy'

--- app/api/v1/routes/auth.py
+++ app/api/v1/routes/auth.py
@@ -82,14 +82,14 @@ async def get_current_user(
 async def login_for_access_token(
     form_data: UserRequestSchema, db: Session = Depends(get_db)
 ):
+    user = authenticate_user(db, form_data.email, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     try:
-        user = authenticate_user(db, form_data.email, form_data.password)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token = create_access_token(
             data={"sub": user.email}, expires_delta=access_token_expires
