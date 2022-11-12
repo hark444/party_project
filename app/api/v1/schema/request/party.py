e2616a31151d73852b8248a63d7fe392116a00b0
Harshad Keshwani <hark44>
2022-11-13 02:54:21 +0530

    Updated party create request schema

--- app/api/v1/schema/request/party.py
+++ app/api/v1/schema/request/party.py
@@ -2,11 +2,14 @@ from pydantic.schema import datetime
 from app.api.v1.schema.request.base import TimeStampRequestSchema
 
 
-class PartyRequestSchema(TimeStampRequestSchema):
+class PartyRequestCreateSchema(TimeStampRequestSchema):
     reason: str = None
     proposed_date: datetime
     guests_invited: int
     party_date: datetime | None = None
     party_place: str | None = None
+
+
+class PartyRequestSchema(PartyRequestCreateSchema):
     ratings: float | None = None
     approved: bool = False
