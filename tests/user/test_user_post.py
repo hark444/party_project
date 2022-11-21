dceafe5d905eee6fbbdbbb5071d47e7055123952
Harshad Keshwani <hark44>
2022-11-21 19:12:24 +0530

    Fixed existing test cases

--- tests/user/test_user_post.py
+++ tests/user/test_user_post.py
@@ -11,7 +11,7 @@ DEFAULT_USER_PAYLOAD = {
 
 
 def test_create_user(client):
-    response = client.post(f"/api/v1/users/create", data=DEFAULT_USER_PAYLOAD)
+    response = client.post(f"/api/v1/users", json=DEFAULT_USER_PAYLOAD)
     assert response.status_code == status.HTTP_200_OK
     response_data = json.loads(response.text)
     assert response_data.get("email") == DEFAULT_USER_PAYLOAD.get("email")
@@ -20,7 +20,7 @@ def test_create_user(client):
 
 def test_create_user_with_missing_email(client):
     del DEFAULT_USER_PAYLOAD["email"]
-    response = client.post(f"/api/v1/users/create", data=DEFAULT_USER_PAYLOAD)
+    response = client.post(f"/api/v1/users", json=DEFAULT_USER_PAYLOAD)
     response_data = json.loads(response.text)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert response_data == {
