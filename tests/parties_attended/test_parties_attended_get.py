55c00260c4f0ef58de59b3a5d0a199ed31d83064
Harshad Keshwani <hark44>
2022-12-10 14:22:20 +0530

    Black formatting

--- tests/parties_attended/test_parties_attended_get.py
+++ tests/parties_attended/test_parties_attended_get.py
@@ -15,7 +15,7 @@ DEFAULT_PARTY_PAYLOAD = {
 DEFAULT_PARTIES_ATTENDED_PAYLOAD = {
     "rating": 5,
     "approved": True,
-    "comment": "Awesome party bro!"
+    "comment": "Awesome party bro!",
 }
 party_url = "/api/v1/party"
 parties_attended_url = "/api/v1/parties_attended"
@@ -28,8 +28,10 @@ def create_party_object(client, headers):
 
 
 def create_party_attended_object(client, headers, party_obj):
-    DEFAULT_PARTIES_ATTENDED_PAYLOAD['party_id'] = party_obj.get("id")
-    party_attended_obj = client.post(parties_attended_url, headers=headers, json=DEFAULT_PARTIES_ATTENDED_PAYLOAD)
+    DEFAULT_PARTIES_ATTENDED_PAYLOAD["party_id"] = party_obj.get("id")
+    party_attended_obj = client.post(
+        parties_attended_url, headers=headers, json=DEFAULT_PARTIES_ATTENDED_PAYLOAD
+    )
     assert party_attended_obj.status_code == 200
     return party_attended_obj
 
@@ -48,12 +50,18 @@ def test_get_single_party_attended(client, account_user_and_token):
     headers = {"Authorization": f'Bearer {account_user_and_token.get("access_token")}'}
     party_obj = create_party_object(client, headers)
     party_attended_obj = create_party_attended_object(client, headers, party_obj)
-    get_party_attended_url = parties_attended_url + f"/{party_attended_obj.json().get('id')}"
+    get_party_attended_url = (
+        parties_attended_url + f"/{party_attended_obj.json().get('id')}"
+    )
     response = client.get(get_party_attended_url, headers=headers)
     assert response.status_code == 200
     response_json = response.json()
-    assert response_json.get("comment") == DEFAULT_PARTIES_ATTENDED_PAYLOAD.get("comment")
-    assert response_json.get("approved") == DEFAULT_PARTIES_ATTENDED_PAYLOAD.get("approved")
+    assert response_json.get("comment") == DEFAULT_PARTIES_ATTENDED_PAYLOAD.get(
+        "comment"
+    )
+    assert response_json.get("approved") == DEFAULT_PARTIES_ATTENDED_PAYLOAD.get(
+        "approved"
+    )
 
 
 def test_get_single_party_with_incorrect_party_id(client, account_user_and_token):
