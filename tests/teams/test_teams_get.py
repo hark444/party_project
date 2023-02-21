65b83bfff4ea44308662f9b8a4cbd7c840c6dfb0
Harshad Keshwani <hark44>
2023-02-21 12:54:12 +0530

    Black formatting

--- tests/teams/test_teams_get.py
+++ tests/teams/test_teams_get.py
@@ -18,8 +18,11 @@ def test_get_all_teams(client):
     assert response.status_code == status.HTTP_200_OK
 
     response_json = response.json()
-    assert response_json.get('total') == 1
-    assert response_json.get('data')[0].get('team_name') == DEFAULT_TEAMS_PAYLOAD['team_name'].lower()
+    assert response_json.get("total") == 1
+    assert (
+        response_json.get("data")[0].get("team_name")
+        == DEFAULT_TEAMS_PAYLOAD["team_name"].lower()
+    )
 
 
 # Since auth layer is open for teams, this test should pass
@@ -32,16 +35,16 @@ def test_get_teams_without_auth(client):
 def test_get__team_by_id(client):
     teams_obj = create_teams(client)
     team_id = teams_obj.get("id")
-    response = client.get(teams_url + f'/{team_id}')
+    response = client.get(teams_url + f"/{team_id}")
 
     assert response.status_code == status.HTTP_200_OK
     response_json = response.json()
-    assert response_json.get('team_name') == DEFAULT_TEAMS_PAYLOAD['team_name'].lower()
+    assert response_json.get("team_name") == DEFAULT_TEAMS_PAYLOAD["team_name"].lower()
 
 
 def test_get_team_with_incorrect_id(client):
     teams_obj = create_teams(client)
     team_id = teams_obj.get("id")
-    response = client.get(teams_url + f'/{team_id+1}')
+    response = client.get(teams_url + f"/{team_id+1}")
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
