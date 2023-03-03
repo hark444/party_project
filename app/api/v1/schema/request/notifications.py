8d1abe3d6e2c148b5c8197aa67d54f23ca4123f8
Harshad Keshwani <hark44>
2023-03-03 13:12:33 +0530

    GET handler for notifications

--- app/api/v1/schema/request/notifications.py
+++ app/api/v1/schema/request/notifications.py
@@ -4,9 +4,18 @@ from app.api.v1.schema.request.base import TimeStampRequestSchema
 from models.notifications import NotificationTypeEnum
 
 
-class NotificationCreateSchema(TimeStampRequestSchema):
+class NotificationsBaseSchema(TimeStampRequestSchema):
+    is_read: bool | None = False
+    expired: bool | None = False
+
+
+class NotificationCreateSchema(NotificationsBaseSchema):
     user_id: int
     type: NotificationTypeEnum
     type_id: int
-    is_read: bool | None = False
-    expired: bool | None = False
+
+
+class NotificationGetSchema(NotificationsBaseSchema):
+    user_id: int | None
+    type: NotificationTypeEnum | None
+    type_id: int | None
